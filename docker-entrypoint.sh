b9ffca920fc1d426eb0ba0353e0c22516d9d865e
Soyouz <soyouz@src386.org>
2022-01-09 21:32:25 +0100

    fix /etc/ssl/private dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@
 # Detect if PluXml has existing data
 if [ ! -e "/etc/ssl/certs/crt.pem" ]; then
     echo "No certificate found. Generating a 10y self-signed..."
+    mkdir /etc/ssl/private
     openssl req -new -x509 -days 3650 -nodes -sha256 \
     -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=localhost" \
     -out /etc/ssl/certs/crt.pem \
